a7dfa0d11980cc739f5dfc843c0920fb007a3495
Derrick McKee <derrick.mckee@gmail.com>
2019-12-17 12:04:07 -0500

    latest

--- init.sh
+++ init.sh
@@ -17,11 +17,47 @@ make_symlink () {
 	ln -s $orig_path $dest_path
 }
 
+# Usage: get_latest_release "repo_name" "install_dir"
+get_latest_release() {
+	gh_release=$(curl --silent "https://api.github.com/repos/$1/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+	if [ -z $gh_release ]; then
+		gh_release="master"
+	fi	
+
+	echo "Fetching release $gh_release from $1"
+	if [ -d $2/$1 ]; then
+		cd $2/$1
+		git fetch origin
+		git checkout $gh_release
+	else
+		if [ ! -d $2/$1 ]; then
+			mkdir -p $2/$1
+		fi
+
+		git clone git@github.com:$1.git $2/$1
+		cd $2/$1
+		git checkout $gh_release
+	fi
+}
+
 # Create symlinks to config files
 for sym in $(cat $DOTFILE_DIR/symlinks.txt); do
 	make_symlink $DOTFILE_DIR/$sym $HOME/$sym
 done
 
+source $HOME/.bashrc
+
 echo "Updating packages"
 sudo apt update
 sudo apt upgrade
+
+packages=$(cat $DOTFILE_DIR/packages.txt | tr -s "\n" " ")
+sudo apt install $packages
+
+if [ ! -f $HOME/.ssh/id_rsa ]; then
+	ssh-keygen
+fi
+
+for repo in $(cat repos.txt); do
+	get_latest_release "$repo" "$DOTFILE_DIR/code"
+done
