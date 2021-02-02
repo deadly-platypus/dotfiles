574b867722d9eefe3c7e84d3f57150b878f2eddf
Derrick McKee <derrick.mckee@gmail.com>
2021-02-01 20:54:53 -0500

    simplifying init script and removing invalid packages

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DOTFILE_DIR=$(dirname $(realpath $0))
-PACKAGE_INSTALL_CMD="pacman -Syu"
-PACKAGE_UPDATE_CMD="pacman -Syu"
+PACKAGE_INSTALL_CMD="pamac install"
+PACKAGE_UPDATE_CMD="pamac update -a"
 
 # Usage: exit_fail "Something happened" 1
 exit_fail() {
@@ -60,14 +60,6 @@ for sym in $(cat $DOTFILE_DIR/symlinks.txt); do
 	make_symlink $DOTFILE_DIR/$sym $HOME/$sym
 done
 
-run_cmd sudo cp $DOTFILE_DIR/sudo_config /etc/sudoers.d/sudo_config
-run_cmd sudo chown root:root /etc/sudoers.d/sudo_config
-
-run_cmd source $HOME/.bashrc
-
-# Not sure if piping works with run_cmd, so purposefully just run the command
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
 echo "Updating packages"
 run_cmd sudo $PACKAGE_UPDATE_CMD
 
@@ -78,25 +70,6 @@ run_cmd sudo $PACKAGE_INSTALL_CMD $packages
 if [ ! -f $HOME/.ssh/id_rsa ]; then
 	run_cmd ssh-keygen
 fi
-
-for repo in $(cat repos.txt); do
-	if [ ! -f $DOTFILE_DIR/build_cmds/$repo ]; then
-		echo "No build commands for $repo"
-		exit 1
-	fi
-	get_latest_release "$repo" "$DOTFILE_DIR/code"
-	if [[ -f $DOTFILES_DIR/generated/$repo ]]; then
-		echo "Removing $(cat $DOTFILES_DIR/generated/$repo)"
-		run_cmd sudo rm -rf $(cat $DOTFILES_DIR/generated/$repo)
-	fi
-	run_cmd cd $DOTFILE_DIR/code/$repo
-	echo -n "Building $repo..."
-	while IFS= read -r build_cmd; do
-		run_cmd $build_cmd
-	done < $DOTFILE_DIR/build_cmds/$repo
-	echo "done."
-	cd $DOTFILE_DIR
-done
 
 clear
 echo "System successfully set up!"
